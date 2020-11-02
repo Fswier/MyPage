d241fc6a6e3d5031d89cb79590de70f796141e2a
Filip Świercz <filipswiercz97@gmail.com>
2020-11-02 01:23:32 +0100

    JS add detailed css links repair in html

---
+++ kontakt/main.js
@@ -0,0 +1,33 @@
+const form = document.querySelector("form");
+const inputName = form.querySelector("input[name=name]");
+const inputEmail = form.querySelector("input[name=email]");
+const btn = form.querySelector("button")
+
+// const inputNameValue = inputName.value.trim()
+// const inputEmailValue = inputEmail.value.trim()
+
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    if (inputName.value.length >0 && inputEmail.value.length >0) {
+        e.target.submit();
+        alert("message sent");
+        return false;
+    } else {
+        // alert("Wrong form filling");
+        
+    }
+})
+
+    
+    form.addEventListener("input", e => {
+    if (inputName.value.length>0 && inputEmail.value.length > 0) {
+        btn.removeAttribute('disabled');
+    // } else if 
+    //  (inputName.value==="" || inputEmail.value === "") {
+    //     btn.setAttribute('disabled','disabled');
+    } else { 
+        btn.setAttribute('disabled','disabled');
+    }
+}
+)
