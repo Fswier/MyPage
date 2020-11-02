b22f7ecfe9b806395ab27328a7998aebe5eb7948
Filip Świercz <filipswiercz97@gmail.com>
2020-11-02 12:38:16 +0100

    language correction css details html details

--- kontakt/main.js
+++ kontakt/main.js
@@ -2,23 +2,25 @@ const form = document.querySelector("form");
 const inputName = form.querySelector("input[name=name]");
 const inputEmail = form.querySelector("input[name=email]");
 const btn = form.querySelector("button")
-
+let end = false;
 // const inputNameValue = inputName.value.trim()
 // const inputEmailValue = inputEmail.value.trim()
 
 
 form.addEventListener("submit", e => {
     e.preventDefault();
+
     if (inputName.value.length >0 && inputEmail.value.length >0) {
-        e.target.submit();
+        e.target.submit()
         alert("message sent");
-        return false;
+        return end
+        
     } else {
         // alert("Wrong form filling");
         
     }
 })
-
+form.addEventListener('submit',send)
     
     form.addEventListener("input", e => {
     if (inputName.value.length>0 && inputEmail.value.length > 0) {
